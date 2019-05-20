dca5eeec1dd19d2f610e61dded69418e43c9fc17
smart05 <hectorg0g@gmail.com>
2019-05-20 10:52:55 -0500

    footer

--- Layouts/footer.php
+++ Layouts/footer.php
@@ -1,5 +1,5 @@
 <div id="footer" class="bg-black small text-center text-white-50 fixed-bottom">
       <div class="container">
-         <p class="text-muted credit">Copyright &copy; Procesos & Tecnología S.A.S 2018</span></p>
+         <p class="text-muted credit">Copyright &copy; Procesos & Tecnología S.A.S</span></p>
      </div>
  </div>
