1f05c1d64dc6e4f0697c1f404306cbb61c1cf7e8
smart05 <hectorg0g@gmail.com>
2019-05-21 08:45:06 -0500

    metadata

--- Clientes/metadata.php
+++ Clientes/metadata.php
@@ -1,17 +1,34 @@
 <?php
-	$urlIp = ""; //IP privada v4
-	$urlZone = ""; //La zona de disponibilidad en la que se ha lanzado la instancia.
-	$urlIpPub = ""; //IP publica v4
-
+$urlip= "http://169.254.169.254/latest/meta-data/local-ipv4"; //IP privada v4
+$urlzone = "http://169.254.169.254/latest/meta-data/placement/availability-zone"; //La zona de disponibilidad en la que se ha lanzado la instancia.
+$urlippub = "http://169.254.169.254/latest/meta-data/public-ipv4"; //IP publica v4
+/*
 $equipo=	$array = [
-		"urlip" => "http://169.254.169.254/latest/meta-data/local-ipv4", //IP privada v4
-		"urlzone" => "http://169.254.169.254/latest/meta-data/placement/availability-zone", //La zona de disponibilidad en la que se ha lanzado la instancia.
-		"urlippub" => "http://169.254.169.254/latest/meta-data/public-ipv4", //IP publica v4
+		//"urlip" => "http://169.254.169.254/latest/meta-data/local-ipv4", //IP privada v4
+		//"urlzone" => "http://169.254.169.254/latest/meta-data/placement/availability-zone", //La zona de disponibilidad en la que se ha lanzado la instancia.
+		//"urlippub" => "http://169.254.169.254/latest/meta-data/public-ipv4", //IP publica v4
+
+		"urlip" => "http://e2ew.inttegrio-aws.com/EICW/services/servicesEIC?wsdl", //IP privada v4
+		"urlzone" => "http://e2ew.inttegrio-aws.com/bmi/services/servicesBMI?wsdl", //La zona de disponibilidad en la que se ha lanzado la instancia.
+		"urlippub" => "http://e2ew.inttegrio-aws.com/mBank/services/servicesMB?wsdl", //IP publica v4
 ];
+*/
+$ip= function($urlip);
+
+function metadatos($url){
+	$handler = curl_init();
+	curl_setopt($handler, CURLOPT_URL, $url);
+	curl_setopt($handler, CURLOPT_RETURNTRANSFER, 1);
+	//$urlIp = curl_exec ($handler);
+	 return curl_exec ($handler);
+	 curl_close ($handler);
+}
+
+var_dump($ip);
 //var_dump($equipo);
 
 	//$long= count ($array);
-		$handler = curl_init();
+		//$handler = curl_init();
 /*
 for ($i =0; $i < $long; $i++){
 
@@ -19,7 +36,7 @@ for ($i =0; $i < $long; $i++){
 	echo "<br>";
 }
 */
-
+/*
 foreach($equipo as $url=>$urldata)
 	{
 		curl_setopt($handler, CURLOPT_URL, $urldata);
@@ -29,9 +46,10 @@ foreach($equipo as $url=>$urldata)
 	//echo "El " . $url . " es " . $urldata;
 	//echo "<br>";
 	}
-var_dump($urlIp);
+	*/
 
-		curl_close ($handler);
+
+
 
 		//$ip = getenv('USER');
 		//$ip = $_SERVER['USER'];
