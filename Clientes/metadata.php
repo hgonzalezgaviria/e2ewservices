c84d7867188869ed7a74eea66f421b674bdb6d9b
smart05 <hectorg0g@gmail.com>
2019-05-21 08:13:25 -0500

    metadata

--- Clientes/metadata.php
+++ Clientes/metadata.php
@@ -1,13 +1,36 @@
-<?php 
-	//carga la plantilla con la header y el footer
-	//require_once('Layouts/layout.php');	
-				$varurl='hola';
-			$url = "http://169.254.169.254/latest/meta-data/local-ipv4";
+<?php
+	$urlIp = ""; //IP privada v4
+	$urlZone = ""; //La zona de disponibilidad en la que se ha lanzado la instancia.
+	$urlIpPub = ""; //IP publica v4
 
-		$handler = curl_init();		
-		curl_setopt($handler, CURLOPT_URL, $url);
+$equipo=	$array = [
+		"urlip" => "http://169.254.169.254/latest/meta-data/local-ipv4", //IP privada v4
+		"urlzone" => "http://169.254.169.254/latest/meta-data/placement/availability-zone", //La zona de disponibilidad en la que se ha lanzado la instancia.
+		"urlippub" => "http://169.254.169.254/latest/meta-data/public-ipv4", //IP publica v4
+];
+//var_dump($equipo);
+
+	//$long= count ($array);
+		$handler = curl_init();
+/*
+for ($i =0; $i < $long; $i++){
+
+	echo $array[$i];
+	echo "<br>";
+}
+*/
+
+foreach($equipo as $url=>$urldata)
+	{
+		curl_setopt($handler, CURLOPT_URL, $urldata);
 		curl_setopt($handler, CURLOPT_RETURNTRANSFER, 1);
-		$out = curl_exec ($handler);
+	  //$urlIp = curl_exec ($handler);
+		$urlIp = array(curl_exec ($handler));
+	//echo "El " . $url . " es " . $urldata;
+	//echo "<br>";
+	}
+var_dump($urlIp);
+
 		curl_close ($handler);
 
 		//$ip = getenv('USER');
